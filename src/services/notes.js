b99140e8e53c676402c8fc0e57b4f05701927082
Yinan Li <gabriel.li5566@gmail.com>
2022-05-24 13:35:23 +0300

    deploy

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+// const baseUrl = 'http://localhost:3001/notes'
 // const baseUrl = 'https://yinan-notes-app.herokuapp.com/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
